48c6d367be022575ae154a43335fa9b3c4b8a0a7
eachingateb <eachingateb@unal.edu.co>
2023-08-14 12:37:37 -0500

    New RT filter for JoinSpectra

--- Functions/JoinSpectra.py
+++ Functions/JoinSpectra.py
@@ -2,13 +2,13 @@ from NeoPower import *
 import numpy as np
 #Some signals are repeated some middle values are used in the left and the right
 #Include intensities
-def JoinSpectra(DataSet,min_mz=200,max_mz=300,minInt=1e2,ML=1,MinDif=2.003,MaxDif=2.005):
+def JoinSpectra(DataSet,min_mz=200,max_mz=300,minInt=1e2,ML=1,MinDif=2.0038,MaxDif=2.0046,min_RT=0,max_RT=100):
     FirstSpectrum=True
     c=0
     for spectrum in DataSet:
         MSLevel=spectrum.getMSLevel()
         RT=spectrum.getRT()/60      
-        if MSLevel==ML: #Checking the MSLevel for the analysis
+        if ((MSLevel==ML)&(RT>min_RT)&(RT<max_RT)): #Checking the MSLevel for the analysis
             RawSpectrum=np.array(spectrum.get_peaks()).T
             SpectrumFilter=np.where((RawSpectrum[:,0]>min_mz)&(RawSpectrum[:,0]<max_mz)&(RawSpectrum[:,1]>minInt))[0]
             SpectrumFil=RawSpectrum[SpectrumFilter,:].copy()
@@ -18,11 +18,9 @@ def JoinSpectra(DataSet,min_mz=200,max_mz=300,minInt=1e2,ML=1,MinDif=2.003,MaxDi
             DifTable=np.c_[DifTable,c*np.ones(LDifTable)]            
             if len(DifTable)>0:
                 if FirstSpectrum:
-                    #JoinedSpectra=SpectrumFil.copy()
                     JoinedSpectra=DifTable.copy()
                     FirstSpectrum=False
                 else:
-                    #JoinedSpectra=np.append(JoinedSpectra,SpectrumFil,axis=0)
                     JoinedSpectra=np.append(JoinedSpectra,DifTable,axis=0)
         c+=1
-    return JoinedSpectra        
+    return JoinedSpectra 
